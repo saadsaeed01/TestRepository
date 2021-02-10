eeb2029f1f5852ec909204b3657c3c5e9017d9b2
Saad Saeed <saad.saeed@tspirits.com>
2021-02-10 11:33:43 +0500

    - put my first commit

--- ATestApp/ConsoleApp1/Program.cs
+++ ATestApp/ConsoleApp1/Program.cs
@@ -4,6 +4,7 @@ namespace ConsoleApp1
 {
     class Program
     {
+        //This is my first comment
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
